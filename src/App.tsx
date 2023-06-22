9014315ee62e584ef07588fb3a722cec47af277f
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-06-22 14:46:56 +0300

    Lesson 11

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,36 @@ import React from 'react';
 import './App.css';
 
 
-const App = () => {
+export const App = () => {
     return (
         <div className="app-wrapper">
-            <header className="header">
-                <img alt='image'
-                    src="https://img.freepik.com/free-vector/vintage-monochrome-serious-russian-bear_225004-583.jpg?w=740&t=st=1685478986~exp=1685479586~hmac=0021ba2cdd87fcb6e5c991da0b6e8a0e4cfdb96abf7ca88f76c95a074b26beb6"/>;
+            <header className={'header'}>
+                <img  src="https://assets.turbologo.com/blog/en/2019/11/19084834/gaming-logo-cover.jpg"
+                     alt=""/>
             </header>
-            <nav className="nav">
+            <nav className={'nav'}>
                 <div><a>Profile</a></div>
                 <div><a>Messages</a></div>
                 <div><a>News</a></div>
                 <div><a>Music</a></div>
                 <div><a>Settings</a></div>
-                <div></div>
-            </nav>
 
-            <div className="content">
-                <div>
-                    <img alt ="image" src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
-                </div>
-                <div>
-                    ava+description
-                </div>
-                <div>
-                    My Post
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        Post1
-                    </div>
-                    <div>
-                        Post1
-                    </div>
-                </div>
+              
+            </nav>
+            <div className={'content'}>
+               <div>
+                   <img src="https://cutewallpaper.org/22/wallpaper-for-ps3-hd/1038611218.jpg" alt=""/>
+               </div>
+                <div>ava+description</div>
+                <div>My Post <div>New Post</div></div>
+
+                <div>post1</div>
+                <div>post2</div>
+
             </div>
         </div>
     );
 }
 
 
-export default App;
+
