a98734761775248972933eb4d7f825484ec7b6cb
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-06-23 23:45:53 +0300

    Lesson 14

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
  export const Header = () => {
     return (
-        <header className={'header'}>
+        <header className={s.header}>
             <img src="https://assets.turbologo.com/blog/en/2019/11/19084834/gaming-logo-cover.jpg"
                  alt=""/>
         </header>
--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import s from  './Profile.module.css'
 
  export const Profile = () => {
     return (
-        <div className={'content'}>
+        <div className={s.content}>
             <div>
                 <img src="https://cutewallpaper.org/22/wallpaper-for-ps3-hd/1038611218.jpg" alt=""/>
             </div>
             <div>ava+description</div>
             <div>My Post <div>New Post</div></div>
 
-            <div>post1</div>
-            <div>post2</div>
+            <div className={s.item}>post1</div>
+            <div className={s.item}>post2</div>
 
         </div>
     )
