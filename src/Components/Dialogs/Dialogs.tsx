19ec26a544fa436a37e84e3d0b6f89bd6949b53d
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-07-22 23:36:05 +0300

    Lesson 24

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -23,17 +23,19 @@ export const Dialogs = () => {
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
 
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
-                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
+
+
+                {/*<DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>*/}
+                {/*<DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>*/}
+                {/*<DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>*/}
+                {/*<DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>*/}
+                {/*<DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>*/}
+                {/*<DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>*/}
 
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
+                {/*<Message message={messagesData[0].message}/>*/}
+                {/*<Message message={messagesData[1].message}/>*/}
             </div>
         </div>
     );
