b4c41cb85bf75c3a7a873f95e7bac7de7a24559c
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-07-23 00:10:53 +0300

    Lesson 25

--- src/Components/Profile/MePosts/MyPosts.tsx
+++ src/Components/Profile/MePosts/MyPosts.tsx
@@ -3,18 +3,19 @@ import s from './MyPosts.module.css'
 import {Post} from './Posts/Post';
 
 export const MyPosts = () => {
-    let postsData = [
+    let post = [
         {id: 1, message: 'Hi,how are you', likesCount: 5},
         {id: 2, message: 'It\'s my first post',likesCount: 13},
     ]
+
+    let postsElements=post.map(p=> <Post message={p.message} like={p.likesCount}/>)
 
     return (
         <div>
             <div className={s.postsBlock}>My Post</div>
             <div>New Post</div>
 
-            <Post message={postsData[0].message} like={postsData[0].likesCount}/>
-            <Post message={postsData[1].message} like={postsData[1].likesCount}/>
+            {postsElements}
 
 
         </div>
